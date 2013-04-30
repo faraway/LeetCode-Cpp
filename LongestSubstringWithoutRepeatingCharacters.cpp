fb2021751d3ae798435f47403ebd2bff36d1d9aa
Yuan <patrickyao1988@gmail.com>
2013-04-29 20:11:55 -0700

    Update LongestSubstringWithoutRepeatingCharacters.cpp

--- LongestSubstringWithoutRepeatingCharacters.cpp
+++ LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,3 +1,11 @@
+/**
+ Given a string, find the length of the longest substring without repeating characters. 
+ For example, the longest substring without repeating letters for "abcabcbb" is "abc", 
+ which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+ 
+ NOTE: This implementation is simpler and better than my java implementation
+**/
+
 #include<unordered_map>
 #include<string>
 
@@ -17,11 +25,13 @@ public:
         for(int i=1;i<s.size();i++){
             unordered_map<char,int>::const_iterator entry = map.find(s[i]);
             if(entry == map.end())
-                map.insert(pair<char,int>(s[i],i));
+                map.emplace(s[i],i);
             else{
                 int temp = entry->second;
                 map.erase(entry);
-                map.insert(pair<char,int>(s[i],i));
+                //map.insert(pair<char,int>(s[i],i)); Copy operation is performed for insert().
+                //emplace() inserts element in-place. i.e. no copy or move operations are performed
+                map.emplace(s[i],i);
                 //if < start, means the former repeated char is out of current range.
                 if(temp<start)
                     continue;
